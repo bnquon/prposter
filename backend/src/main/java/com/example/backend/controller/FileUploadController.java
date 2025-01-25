9ec84a77b64dc0d28debceb3f537b41cef99e097
Brandon Quon <bqa7@sfu.ca>
2025-01-25 00:16:30 -0800

    clean up unused imports

--- backend/src/main/java/com/example/backend/controller/FileUploadController.java
+++ backend/src/main/java/com/example/backend/controller/FileUploadController.java
@@ -6,7 +6,6 @@ import com.example.backend.service.PostsService;
 import com.example.backend.service.S3FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
